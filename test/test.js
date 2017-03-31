e1c26abb1a7724166fcd74c41507016800d65250
Marian Schubert <marian.schubert@gmail.com>
2017-03-31 21:26:31 +0200

    More basic PUT/GET tests

--- test/test.js
+++ test/test.js
@@ -27,4 +27,26 @@ describe('storage', function () {
         .then(() => client.get(key).expect(content))
     })
   })
+
+  describe('PUT', function () {
+    it('returns ID of stored object (i.e. sha1 hash of its path)', function () {
+      return client.put('/key')
+        .send('content')
+        .expect('bc8f1dc137b325391764fdf8ba7bb1176731e515\n')
+    })
+  })
+
+  describe('GET', function () {
+    it('returns 404 when object is not found', function () {
+      return client.get('/unknown').expect(404)
+    })
+
+    it('can fetch object by its ID (at /o/object-id)', function () {
+      var key = '/item'
+      var content = 'object-id-content'
+
+      return client.put(key).send(content).expect(200)
+        .then(res => client.get('/o/' + res2id(res)).expect(content).expect(200))
+    })
+  })
 })
