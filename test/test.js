0e76471839c8ff2494b29ca86609ed1e212fa722
Marian Schubert <marian.schubert@gmail.com>
2017-03-31 20:49:02 +0200

    Write basic test for PUT/GET

--- test/test.js
+++ test/test.js
@@ -1,11 +1,30 @@
 /* eslint-env node, mocha */
 var request = require('supertest')
 var app = require('../src/app.js')
+var config = require('../src/config.js')
+var tmp = require('tmp')
+
+var client = request(app)
 
 describe('homepage', function () {
   it('should return s2', function () {
     return request(app)
       .get('/')
       .expect('s2')
+  })
+})
+
+describe('storage', function () {
+  // Make sure we use clean storage for each test
+  beforeEach(() => { config.dataDir = tmp.dirSync().name })
+
+  describe('HTTP PUT/GET', function () {
+    it('stores/retrieves content', function () {
+      var key = '/item'
+      var content = 'hello'
+
+      return client.put(key).send(content).expect(200)
+        .then(() => client.get(key).expect(content))
+    })
   })
 })
