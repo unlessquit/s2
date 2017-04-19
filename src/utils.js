828aa97d56623e803d3268c77e5b2ad406a89c61
Marian Schubert <marian.schubert@gmail.com>
2017-04-19 13:53:50 +0200

    Remove unused maybeGetMetadata function

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,9 @@
-var fs = require('fs')
-
 function uuid () {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0
     var v = c === 'x' ? r : (r & 0x3 | 0x8)
     return v.toString(16)
   })
-}
-
-function maybeGetMetadata (filename) {
-  try {
-    return JSON.parse(fs.readFileSync(filename))
-  } catch (e) {
-    return null
-  }
 }
 
 /**
@@ -33,5 +23,4 @@ function removeContentHeaderFields (res) {
 }
 
 exports.uuid = uuid
-exports.maybeGetMetadata = maybeGetMetadata
 exports.removeContentHeaderFields = removeContentHeaderFields
