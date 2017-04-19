660b3b1ae5fd33ff21ecaa1eddb9f2c9bc5b201f
Marian Schubert <marian.schubert@gmail.com>
2017-04-19 09:46:32 +0200

    Extract code which touches data from app.js into S2Obj

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 var fs = require('fs')
 var path = require('path')
-var mime = require('mime-types')
 var express = require('express')
 var fresh = require('fresh')
 var app = express()
-var crypto = require('crypto')
-var mkdirp = require('mkdirp')
-var config = require('./config')
 var utils = require('./utils')
 var http = require('./http')
+var S2Obj = require('./s2obj')
 
 // Allow s2.js to do it's stuff
 app.use(function (req, res, next) {
@@ -23,36 +20,22 @@ app.get('/', (req, res) => res.send('s2'))
 app.use('/app', express.static('public'))
 
 // Storage
-var key2id = s => crypto.createHash('sha1').update(s).digest('hex')
-var id2dir = id => path.join(config.dataDir, id.slice(0, 2), id.slice(2, 4))
-
-app.get('/o/:id', (req, res) => sendFile(req.params.id, req, res, {anonymize: true}))
+app.get('/o/:id', (req, res) => {
+  return S2Obj.loadById(req.params.id)
+    .then(obj => sendFile(obj, req, res, {anonymize: true}))
+})
 
 app.get(/.*/, (req, res) => {
-  var key = req.path
-  var id = key2id(key)
-
-  sendFile(id, req, res)
+  return S2Obj.loadByKey(req.path)
+    .then(obj => sendFile(obj, req, res))
 })
 
 app.put(/.+/, (req, res) => {
   var asInbox = req.query.as === 'inbox'
   var key = asInbox ? path.join(req.path, utils.uuid()) : req.path
-  var id = key2id(key)
-  var dir = id2dir(id)
 
-  mkdirp(dir, err => {
-    if (err) {
-      http.error500(res, JSON.stringify(err))
-      return
-    }
-
-    var filename = path.join(dir, id)
-    var filenameMetadata = filename + '.json'
-
-    var m = utils.maybeGetMetadata(filenameMetadata)
-
-    if (m && req.headers['if-match'] && m.etag !== req.headers['if-match']) {
+  S2Obj.loadByKey(key).then(obj => {
+    if (!obj.validatePutCondition(req.headers)) {
       http.error412(res)
       return
     }
@@ -63,66 +46,39 @@ app.put(/.+/, (req, res) => {
       'etag': utils.uuid()
     }
 
-    fs.writeFile(filenameMetadata, JSON.stringify(metadata), err => {
-      if (err) {
-        http.error500(res, JSON.stringify(err))
-        return
-      }
-
-      res.set('ETag', metadata.etag)
-
-      var writer = fs.createWriteStream(filename)
-      var pipe = req.pipe(writer)
-      pipe.on('error', err => {
-        console.error('ERROR Failed to write', filename, 'Reason:', err)
+    obj.write(metadata, req)
+      .then(() => {
+        res.set('ETag', metadata.etag)
+        res.send(obj.id + '\n')
+      })
+      .catch(err => {
+        console.error('ERROR Failed to write', obj.filename, 'Reason:', err)
         http.error500(res, JSON.stringify(err))
       })
-      pipe.on('finish', () => res.send(id + '\n'))
-    })
   })
 })
 
-function sendFile (id, req, res, opts) {
+function sendFile (obj, req, res, opts) {
   opts = opts || {}
-  var filename = path.join(id2dir(id), id)
 
-  fs.readFile(filename + '.json', (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        http.error404(res)
-        return
-      }
+  if (!obj.metadata.key) {
+    http.error404(res)
+    return
+  }
 
-      http.error500(res, JSON.stringify(err))
-      return
-    }
+  var stat = fs.statSync(obj.filename)
 
-    var metadata = JSON.parse(data)
-    var originalFilename = opts.anonymize
-        ? anonymizedFilename(id, metadata)
-        : path.basename(metadata.key)
-    var stat = fs.statSync(filename)
+  res.set('Content-Type', obj.metadata['content-type'])
+  res.set('Content-Disposition', 'inline; filename="' +
+          (opts.anonymize ? obj.anonymizedName : obj.name) + '"')
+  res.setHeader('Content-Length', stat.size)
+  res.setHeader('ETag', obj.metadata.etag || 'n/a')
+  if (fresh(req.headers, res._headers)) {
+    http.notModified(res)
+    return
+  }
 
-    res.set('Content-Type', metadata['content-type'])
-    res.set('Content-Disposition', 'inline; filename="' +
-            originalFilename + '"')
-    res.setHeader('Content-Length', stat.size)
-    res.setHeader('ETag', metadata.etag || 'n/a')
-    if (fresh(req.headers, res._headers)) {
-      http.notModified(res)
-      return
-    }
-
-    fs.createReadStream(filename).pipe(res)
-  })
-}
-
-function anonymizedFilename (id, metadata) {
-  var ext = mime.extension(metadata['content-type'])
-
-  return ext
-    ? id + '.' + ext
-    : id
+  fs.createReadStream(obj.filename).pipe(res)
 }
 
 module.exports = app
---
+++ src/s2obj.js
@@ -0,0 +1,103 @@
+var fs = require('fs')
+var mkdirp = require('mkdirp')
+var crypto = require('crypto')
+var mime = require('mime-types')
+var config = require('./config')
+var path = require('path')
+
+var key2id = s => crypto.createHash('sha1').update(s).digest('hex')
+var id2dir = id => path.join(config.dataDir, id.slice(0, 2), id.slice(2, 4))
+
+class S2Obj {
+  static loadById (id) {
+    var obj = new S2Obj(id)
+
+    return new Promise((resolve, reject) => {
+      fs.readFile(obj.filenameMetadata, (err, data) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            resolve(obj)
+            return
+          }
+
+          reject(err)
+          return
+        }
+
+        obj.metadata = JSON.parse(data)
+        resolve(obj)
+      })
+    })
+  }
+
+  static loadByKey (key) {
+    return S2Obj.loadById(key2id(key))
+  }
+
+  static createForKey (key) {
+    return new S2Obj(key2id(key))
+  }
+
+  constructor (id) {
+    this.id = id
+    this.dir = id2dir(id)
+    this.filename = path.join(this.dir, this.id)
+    this.filenameMetadata = path.join(this.dir, this.id) + '.json'
+    this.metadata = {}
+  }
+
+  get name () {
+    return path.basename(this.metadata.key)
+  }
+
+  get anonymizedName () {
+    var ext = mime.extension(this.metadata['content-type'])
+
+    return ext
+      ? this.id + '.' + ext
+      : this.id
+  }
+
+  get metadata () {
+    return this._metadata
+  }
+
+  set metadata (data) {
+    this._metadata = data
+  }
+
+  validatePutCondition (condition) {
+    if (!condition['if-match']) return true
+
+    if (this.metadata.etag !== condition['if-match']) return false
+
+    return true
+  }
+
+  write (metadata, src) {
+    return new Promise((resolve, reject) => {
+      mkdirp(this.dir, err => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        fs.writeFile(this.filenameMetadata, JSON.stringify(metadata), err => {
+          if (err) {
+            reject(err)
+            return
+          }
+
+          this.metadata = metadata
+
+          var writer = fs.createWriteStream(this.filename)
+          var pipe = src.pipe(writer)
+          pipe.on('error', err => reject(err))
+          pipe.on('finish', () => resolve())
+        })
+      })
+    })
+  }
+}
+
+module.exports = S2Obj
